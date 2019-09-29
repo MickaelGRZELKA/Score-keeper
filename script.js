0af1b6f0bd88b17024e3057a6f92cc00fc69248c
Mickael Grzelka <grzelka.mickael@gmail.com>
2019-09-29 17:17:12 +0200

    add function add score after p1 win

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ let p2Score = 0;
 let gameOver = false;
 let scoreToWin = 5;
 let scoreToWinResult = document.querySelector("p span");
+
+
+function addScoreAfterP1Win(){
+    if(!gameOver){
+        p1Score++;
+        if (p1Score === scoreToWin){
+            p1Result.classList.add("winner");
+            gameOver = true;
+        }
+        p1Result.textContent = p1Score;
+    }
+    p1Result.textContent = p1Score;
+}
